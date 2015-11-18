a29d5d32c5aa1489e2b467d96573eb5280ab4df8
Pravdina Elena <helen-j@inventos.ru>
2015-11-18 16:13:53 +0300

    fixes for scroller component (navigation, this for provider)

--- js/components/scroller.js
+++ js/components/scroller.js
@@ -16,7 +16,7 @@ TVComponents.Scroller.prototype.getScrollerText = function() {
 	var name = (TV.app.curr_popup) ? TV.app.curr_popup.name : TV.app.curr_page.name;
 	var ejs_path = 'text#' + name + '.' + this.id;
 	if (!TV.app.ejs[ejs_path]) throw 'Not defined template ' + ejs_path + ' for component ' + this.id;
-	return TV.app.ejs[ejs_path]();
+	return TV.app.ejs[ejs_path](this);
 };
 
 TVComponents.Scroller.prototype._initScrollbarButtons = function() {
@@ -24,9 +24,18 @@ TVComponents.Scroller.prototype._initScrollbarButtons = function() {
 		var el = TV.el(selector, this.el);
 		if (el) {
 			el.setAttribute('data-type', 'button');
+			el.setAttribute('data-id', this.id+'_'+selector.replace(/.*"slider_(.*?)".*/, '$1'));
 			new TVButton(el, this.buttons, this).onclick = function() {
 				this._move(is_first, this.container);
 				this.setScrollbar();
+				var side;
+				if (is_first) {
+					side = this.is_horizontal ? 'left' : 'up';
+				} else {
+					side = this.is_horizontal ? 'right' : 'down';
+				}
+				TVComponent.prototype.oncursor.call(this, side);
+				this.buttons._hover_btn && TV.removeClass(this.buttons._hover_btn.el, TVButton.hover_class);
 			}.bind(this);
 		}
 	}.bind(this);
@@ -55,30 +64,51 @@ TVComponents.Scroller.prototype._move = function(is_move_in_first, container_el)
 	if (this.is_horizontal) {
 		var container_left = parseInt(container_el.style.left) || 0;
 		container_left += (is_move_in_first) ? this.step : -this.step;
-		if (this._checkEnding(container_el, -container_left)) {
+		var end_side = this._checkEnding(container_el, -container_left);
+		this.updateNavButtons(end_side);
+		if (!end_side) {
 			container_el.style.left = container_left + 'px';
 			(is_move_in_first) ? this.count-- : this.count++;
 		} else {
-			return false;
+			return true;
 		}
 	} else {
 		var container_top = parseInt(container_el.style.top) || 0;
 		container_top += (is_move_in_first) ? this.step : -this.step;
-		if (this._checkEnding(container_el, -container_top)) {
+		var end_side = this._checkEnding(container_el, -container_top);
+		this.updateNavButtons(end_side);
+		if (!end_side) {
 			(is_move_in_first) ? this.count-- : this.count++;
 			container_el.style.top = container_top + 'px';
 		} else {
-			return false;
+			return true;
 		}
 	}
 };
+
+TVComponents.Scroller.prototype.updateNavButtons = function(end_side) {
+	if (!end_side) {
+		this.buttons[this.id+'_nav_prev'] && this.buttons[this.id+'_nav_prev'].enable();
+		this.buttons[this.id+'_sc_prev'] && this.buttons[this.id+'_sc_prev'].enable();
+		this.buttons[this.id+'_nav_next'] && this.buttons[this.id+'_nav_next'].enable();
+		this.buttons[this.id+'_sc_next'] && this.buttons[this.id+'_sc_next'].enable();
+	} else if (end_side == 'start') {
+		this.buttons[this.id+'_nav_prev'] && this.buttons[this.id+'_nav_prev'].disable();
+		this.buttons[this.id+'_sc_prev'] && this.buttons[this.id+'_sc_prev'].disable();
+	} else if (end_side == 'end') {
+		this.buttons[this.id+'_nav_next'] && this.buttons[this.id+'_nav_next'].disable();
+		this.buttons[this.id+'_sc_next'] && this.buttons[this.id+'_sc_next'].disable();
+	}
+}
 
 TVComponents.Scroller.prototype._checkEnding = function(el , k) {
 	var boundary = {
 		first: 0,
 		last: (this.is_horizontal) ?  TV.getSize(el).width - TV.getSize(this.frame_el).width + this.step : TV.getSize(el).height - TV.getSize(this.frame_el).height + this.step
 	};
-	return (k >= Math.abs(boundary.first) && k <= Math.abs(boundary.last))
+	if (k <= Math.abs(boundary.first)) return 'start';
+	if (k >= Math.abs(boundary.last)) return 'end';
+	return false;
 };
 
 TVComponents.Scroller.prototype.onready = function() {
@@ -89,6 +119,7 @@ TVComponents.Scroller.prototype.onready = function() {
 	// инициализируем скрол
 	this._initScrollbarButtons();
 	this.setScrollbar();
+	this.updateNavButtons('start');
 };
 
 TVComponents.Scroller.prototype.setScrollbar = function() {
